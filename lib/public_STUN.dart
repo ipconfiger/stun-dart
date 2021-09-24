4e5c37664627ff5586e5a809fe4601d04df499a1
压力很大同志 <superpowerlee@gmail.com>
2021-09-24 12:07:26 +0800

    Update public_STUN.dart

--- lib/public_STUN.dart
+++ lib/public_STUN.dart
@@ -8,6 +8,6 @@ class FullAddress {
 }
 
 List<FullAddress> publicSTUNs = [
-  FullAddress(InternetAddress("75.2.81.221"), 3478)
+  FullAddress(InternetAddress("54.65.46.167"), 3478)
   // new FullAddress(InternetAddress("18.191.223.12"), 3478)
 ];
